e03eb0b80d637e3bdc9e086794072a5fdb91d5a7
OussamaAssassi <oussama.assassi@gmail.com>
2015-02-26 17:02:00 +0100

    remove youtubeIframe directive + put getVideoUrl function in MainCtrl

--- js/controllers/MainCtrl.js
+++ js/controllers/MainCtrl.js
@@ -1,19 +1,19 @@
-app.controller('MainCtrl', ['$scope','youtubeService', function($scope, youtubeService) {
-    
-    $scope.activeVideo = "";
+app.controller('MainCtrl', ['$scope','$timeout','$sce','youtubeService', function($scope, $timeout, $sce, youtubeService) {
     
     $scope.videoList = function(query) {
-
             youtubeService.getSearchList(query).then(function (data) {
                 $scope.list = data;
                 $scope.activeVideo = data[0].id.videoId;
-
-                console.log($scope.activeVideo);
+                $scope.getVideoUrl($scope.activeVideo);
             }, function(error) {
                 console.log(error);
             });
-    }
+    };
     
     $scope.videoList();
+    
+    $scope.getVideoUrl = function(videoId) {
+        $scope.videoUrl = $sce.trustAsResourceUrl('https://www.youtube.com/embed/' + videoId + '?autoplay=1');
+    };
 
 }]);
